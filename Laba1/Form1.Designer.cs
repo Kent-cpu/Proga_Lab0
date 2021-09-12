0c1e04cd7b83382c30cfbc7c2b968b11e001a45c
Руслан Мирзаев <ruslanmirzaev16621@gmail.com>
2021-09-12 22:51:51 +0700

    project initialization

---
+++ Laba1/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿
+namespace Laba1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.aField = new System.Windows.Forms.TextBox();
+            this.bField = new System.Windows.Forms.TextBox();
+            this.hField = new System.Windows.Forms.TextBox();
+            this.mField = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.outputAnswer = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.errorField = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label5 = new System.Windows.Forms.Label();
+            this.findInfromation = new System.Windows.Forms.Button();
+            this.generationDataBtn = new System.Windows.Forms.Button();
+            this.informationUsers = new System.Windows.Forms.RichTextBox();
+            this.errorTask2 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // aField
+            // 
+            this.aField.Location = new System.Drawing.Point(20, 57);
+            this.aField.Name = "aField";
+            this.aField.Size = new System.Drawing.Size(167, 27);
+            this.aField.TabIndex = 0;
+            this.aField.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // bField
+            // 
+            this.bField.Location = new System.Drawing.Point(207, 57);
+            this.bField.Name = "bField";
+            this.bField.Size = new System.Drawing.Size(167, 27);
+            this.bField.TabIndex = 1;
+            this.bField.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // hField
+            // 
+            this.hField.Location = new System.Drawing.Point(395, 54);
+            this.hField.Name = "hField";
+            this.hField.Size = new System.Drawing.Size(167, 27);
+            this.hField.TabIndex = 2;
+            // 
+            // mField
+            // 
+            this.mField.Location = new System.Drawing.Point(585, 54);
+            this.mField.Name = "mField";
+            this.mField.Size = new System.Drawing.Size(167, 27);
+            this.mField.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(207, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "b";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(394, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "h";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(583, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "m";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(287, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 34);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "=";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // outputAnswer
+            // 
+            this.outputAnswer.AutoSize = true;
+            this.outputAnswer.Location = new System.Drawing.Point(356, 262);
+            this.outputAnswer.Name = "outputAnswer";
+            this.outputAnswer.Size = new System.Drawing.Size(18, 20);
+            this.outputAnswer.TabIndex = 9;
+            this.outputAnswer.Text = "g";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(1, -1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(798, 450);
+            this.tabControl1.TabIndex = 10;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.errorField);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.outputAnswer);
+            this.tabPage1.Controls.Add(this.aField);
+            this.tabPage1.Controls.Add(this.bField);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.hField);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.mField);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(790, 417);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // errorField
+            // 
+            this.errorField.AutoSize = true;
+            this.errorField.ForeColor = System.Drawing.Color.Red;
+            this.errorField.Location = new System.Drawing.Point(356, 183);
+            this.errorField.Name = "errorField";
+            this.errorField.Size = new System.Drawing.Size(41, 20);
+            this.errorField.TabIndex = 10;
+            this.errorField.Text = "error";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.errorTask2);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.findInfromation);
+            this.tabPage2.Controls.Add(this.generationDataBtn);
+            this.tabPage2.Controls.Add(this.informationUsers);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(790, 417);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Узнать информацию об абонентах";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 109);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(143, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Данные абонентов";
+            // 
+            // findInfromation
+            // 
+            this.findInfromation.Location = new System.Drawing.Point(310, 22);
+            this.findInfromation.Name = "findInfromation";
+            this.findInfromation.Size = new System.Drawing.Size(277, 47);
+            this.findInfromation.TabIndex = 11;
+            this.findInfromation.Text = "Узнать информацию об абонентах";
+            this.findInfromation.UseVisualStyleBackColor = true;
+            this.findInfromation.Click += new System.EventHandler(this.findInfromation_Click);
+            // 
+            // generationDataBtn
+            // 
+            this.generationDataBtn.Location = new System.Drawing.Point(27, 22);
+            this.generationDataBtn.Name = "generationDataBtn";
+            this.generationDataBtn.Size = new System.Drawing.Size(277, 47);
+            this.generationDataBtn.TabIndex = 1;
+            this.generationDataBtn.Text = "Генерировать";
+            this.generationDataBtn.UseVisualStyleBackColor = true;
+            this.generationDataBtn.Click += new System.EventHandler(this.generationDataBtn_Click);
+            // 
+            // informationUsers
+            // 
+            this.informationUsers.Location = new System.Drawing.Point(27, 141);
+            this.informationUsers.Name = "informationUsers";
+            this.informationUsers.Size = new System.Drawing.Size(740, 247);
+            this.informationUsers.TabIndex = 0;
+            this.informationUsers.Text = "";
+            // 
+            // errorTask2
+            // 
+            this.errorTask2.AutoSize = true;
+            this.errorTask2.Location = new System.Drawing.Point(293, 89);
+            this.errorTask2.Name = "errorTask2";
+            this.errorTask2.Size = new System.Drawing.Size(16, 20);
+            this.errorTask2.TabIndex = 13;
+            this.errorTask2.Text = "с";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 448);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "h";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox aField;
+        private System.Windows.Forms.TextBox bField;
+        private System.Windows.Forms.TextBox hField;
+        private System.Windows.Forms.TextBox mField;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label outputAnswer;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label errorField;
+        private System.Windows.Forms.Button generationDataBtn;
+        private System.Windows.Forms.RichTextBox informationUsers;
+        private System.Windows.Forms.Button findInfromation;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label errorTask2;
+    }
+}
+
---
+++ Laba1/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Laba1
+{
+    public partial class Form1 : Form
+    {
+        List<string> allPhone = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
+            errorField.Text = "";
+            outputAnswer.Text = "";
+            double a, b, h, m;
+
+            if (double.TryParse(aField.Text, out a) && double.TryParse(bField.Text, out b) && double.TryParse(hField.Text, out h) && double.TryParse(mField.Text, out m))
+            {
+
+                double answer = 2 * m / (a * b * h);
+
+                if (a * b * h == 0)
+                {
+                    errorField.Text = "Деление на ноль, проверьте входные данные";
+                }
+                else if (double.IsInfinity(a) || double.IsInfinity(b) || double.IsInfinity(h) || double.IsInfinity(m) || double.IsInfinity(answer))
+                {
+                    errorField.Text = "Слишком большие числа";
+                }
+                else
+                {
+                    outputAnswer.Text = "Ответ: " + answer.ToString();
+                }
+
+            }
+            else
+            {
+                errorField.Text = "Некоректные данные";
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool validate(string[] meaning)
+        {
+
+            return true;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void generationData()
+        {
+            string[] allSurname = { "Смирнов", "Иванов", "Сидоров", "Морозов", "Петров", "Лебедев", "Волков", "Зайцев", "Воробьев" };
+            XmlDocument document = new XmlDocument();
+            XmlDeclaration XmlDec = document.CreateXmlDeclaration("1.0", "utf-8", null);
+            document.AppendChild(XmlDec);
+            XmlElement allSub = document.CreateElement("Allsubscriber");
+            document.AppendChild(allSub);
+
+
+            Random rnd = new Random();
+
+
+            for (int i = 0; i < 50; ++i)
+            {
+                string phoneStr = "", yearStr = "";
+
+
+                for (int j = 0; j < 2; ++j)
+                {
+                    int phone = rnd.Next(0, 9);
+                    int year = rnd.Next(0, 9);
+                    phoneStr += phone.ToString() + phone.ToString();
+                    yearStr += year.ToString() + year.ToString();
+                }
+
+
+                XmlElement subscriber = document.CreateElement("subscriber");
+                XmlElement surname = document.CreateElement("surname");
+                surname.InnerText = allSurname[rnd.Next(0, allSurname.Length - 1)];
+                XmlElement phoneNubmer = document.CreateElement("phoneNumber");
+                phoneNubmer.InnerText = phoneStr;
+                XmlElement yearInstallation = document.CreateElement("yearInstallation");
+                yearInstallation.InnerText = yearStr;
+
+                subscriber.AppendChild(surname);
+                subscriber.AppendChild(phoneNubmer);
+                subscriber.AppendChild(yearInstallation);
+
+
+                document.DocumentElement.InsertAfter(subscriber, document.DocumentElement.LastChild);
+
+            }
+
+
+            document.Save(@"C:\\Users\rusla\Desktop\Sub.xml");
+        }
+
+
+        private void generationDataBtn_Click(object sender, EventArgs e)
+        {
+            generationData();
+        }
+
+        private void findInfromation_Click(object sender, EventArgs e)
+        {
+
+            informationUsers.Text = "";
+            if (!File.Exists(@"C:\\Users\rusla\Desktop\Sub.xml"))
+            {
+                errorTask2.Text = "Файл не создан, сгенерируйте файл";
+                return;
+            }
+
+            string[] textFile = File.ReadAllLines(@"C:\\Users\rusla\Desktop\Sub.xml");
+            if (textFile.Length == 0)
+            {
+                generationData();
+            }
+
+            XmlDocument document = new XmlDocument();
+            document.Load(@"C:\\Users\rusla\Desktop\Sub.xml");
+            int coincidence = 0;
+            XmlElement xRoot = document.DocumentElement;
+
+            foreach (XmlNode xnode in xRoot)
+            {
+                string getPhone = "";
+                string getYear = "";
+                string getSurname = "";
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                    switch (childnode.Name)
+                    {
+                        case "phoneNumber":
+                           getPhone = childnode.InnerText;
+                           break;
+                        case "yearInstallation":
+                            getYear = childnode.InnerText;
+                            break;
+                        case "surname":
+                            getSurname = childnode.InnerText;
+                            break;
+                    }
+                }
+                if (getPhone != "" && getYear != "" &&  getPhone[3] == getYear[3] && getPhone[2] == getYear[2])
+                {
+                    informationUsers.Text += $"Surname: {getSurname} , phone: {getPhone}, year: {getYear}\n";
+                    ++coincidence;
+                }
+            }
+
+            if(coincidence == 0)
+            {
+                informationUsers.Text = "Совпадений не найдено";
+            }
+        }
+    }
+}
